1faeab59b6f2a35e1bf097e0639e0948777244f2
NitheshChandher <nithesh.chandher1997@gmail.com>
2024-04-26 15:56:23 +0200

    update: Tokenizer

--- project/data.py
+++ project/data.py
@@ -16,7 +16,9 @@ import wget
 import zipfile
 import pandas as pd
 from datasets import load_dataset
+import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 from transformers import AutoTokenizer, CLIPTextModel, AutoModel, CLIPModel
 
 class TextEncoder(nn.Module):
@@ -29,6 +31,7 @@ class TextEncoder(nn.Module):
     def forward(self, Xbatch, Xmask):
         ber_out = self.bert_model(input_ids=Xbatch, attention_mask=Xmask)
         return ber_out.last_hidden_state[:, 0, :]
+    
 
 def create_df(features, dataset):
     all_data={}
@@ -49,11 +52,16 @@ def get_dataset(name):
             with zipfile.ZipFile(filename, 'r') as zip_ref:
                 zip_ref.extractall()
         data = pd.read_csv('dredze_amazon_reviews.tsv', sep='\t', header=None, names=['product', 'sentiment', 'text'])
-
+        data = data.rename(columns={'product':'label'})
+        data = data.drop(columns=data.columns.difference(['text', 'label']))
+        
     elif name == 'NC-Dataset':
         dataset = load_dataset("heegyu/news-category-dataset")
         features = list(dataset['train'].features.keys())
         data = create_df(features, dataset)
+        data['text'] = data['headline'].astype(str)+'. '+ data['short_description']
+        data = data.rename(columns={'category':'label'})
+        data = data.drop(columns=data.columns.difference(['text', 'label']))
 
     elif name == 'SST2':
         dataset = load_dataset("stanfordnlp/sst2")
@@ -71,15 +79,35 @@ def get_dataset(name):
         data = create_df(features, dataset)
 
     else:
-        print("Invalid Dataset Name")
+        print("Invalid Dataset Name!")
     return data
 
+def get_dataloader(data, model_name):
+
+    if model_name == 'Bert':
+        tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+    
+    elif model_name == 'Roberta':
+        tokenizer = AutoTokenizer.from_pretrained('roberta-base')
+    
+    elif model_name == 'Xlm_Roberta':
+        tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
+    
+    elif model_name == 'CLIP':
+        tokenizer = AutoTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+    
+    else: 
+        print("Invalid Encoder Name!")
+    
+    return 0 
+
 def main():
-    parser = argparse.ArgumentParser(description="Create Dataloader for a given Dataset")
-    parser.add_argument("--name", type=str, default='Amazon')
+    parser = argparse.ArgumentParser(description="Create a Dataloader for the given Dataset")
+    parser.add_argument("--dname", type=str, default='Amazon')
+    parser.add_argument("--encoder", type=str, default='Bert')
     args = parser.parse_args()
-    name = args.name
-    data = get_dataset(name)
+    data = get_dataset(args.dname)
+    data_loader = get_dataloader(data,args.encoder)
     return data
 
 if __name__== "__main__":
