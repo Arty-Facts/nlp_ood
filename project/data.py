9f358055efeca7575b7d6a797bfb124f7418c481
NitheshChandher <nithesh.chandher1997@gmail.com>
2024-04-29 19:57:05 +0200

    modified:   project/data.py

--- project/data.py
+++ project/data.py
@@ -28,8 +28,8 @@ class TextEncoder(nn.Module):
         self.name = model_name
         self.bert_model = AutoModel.from_pretrained(model_name)
 
-    def forward(self, Xbatch, Xmask):
-        ber_out = self.bert_model(input_ids=Xbatch, attention_mask=Xmask)
+    def forward(self, Xbatch):
+        ber_out = self.bert_model(input_ids=Xbatch)
         return ber_out.last_hidden_state[:, 0, :]
     
 
@@ -82,23 +82,45 @@ def get_dataset(name):
         print("Invalid Dataset Name!")
     return data
 
-def get_dataloader(data, model_name):
+def get_datastream(data, model_name):
 
     if model_name == 'Bert':
         tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-    
+        name = 'bert-base-uncased'
     elif model_name == 'Roberta':
         tokenizer = AutoTokenizer.from_pretrained('roberta-base')
-    
+        name = 'roberta-base'
     elif model_name == 'Xlm_Roberta':
         tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
+        name = 'xlm-roberta-base'
     
     elif model_name == 'CLIP':
         tokenizer = AutoTokenizer.from_pretrained("openai/clip-vit-base-patch32")
     
     else: 
         print("Invalid Encoder Name!")
-    
+    all_text = list(data['text'])
+    tokenize_text = [tokenizer.encode(sentence, add_special_tokens=True) for sentence in all_text]
+    tokenize_text = [idx for lst in tokenize_text for idx in lst]
+    print("Length of the dataset:",len(all_text))
+    print("Total no of words/tokens:", len(tokenize_text))
+    pad_value = 256 - len(tokenize_text) % 256
+    if pad_value != 0:
+        tokenize_text = tokenize_text + [tokenizer.pad_token_id] * pad_value
+    print("Total no of words/tokens after padding:",len(tokenize_text))
+    batch = [torch.tensor(tokenize_text[idk:idk+512]) for idk in range(0,len(tokenize_text),256)]
+    print("Total no of batches:", len(batch))
+    model = TextEncoder(model_name='bert-base-uncased')
+    emb = []
+    with torch.no_grad():
+        model.eval()
+        model.to('cuda')
+        for _batch_ in batch:
+            _batch_ = _batch_.to('cuda').unsqueeze(0)
+            output = model(_batch_).cpu().detach()
+            emb.append(output[0])
+        model.to('cpu')
+    print("Total no of embeddings:", len(emb))
     return 0 
 
 def main():
@@ -107,7 +129,7 @@ def main():
     parser.add_argument("--encoder", type=str, default='Bert')
     args = parser.parse_args()
     data = get_dataset(args.dname)
-    data_loader = get_dataloader(data,args.encoder)
+    data_loader = get_datastream(data,args.encoder)
     return data
 
 if __name__== "__main__":
